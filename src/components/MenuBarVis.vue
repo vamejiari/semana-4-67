38621fdc5a12364c5695d3f77267fe805eed9040
garciaomalejandra <garciaomalejandra@gmail.com>
2020-12-20 01:15:24 -0500

    menu bar visitors

--- src/components/MenuBarVis.vue
+++ src/components/MenuBarVis.vue
@@ -6,6 +6,7 @@
 
       <v-tabs background-color="transparent" color="white" grow>
         <v-tab link to="/homevis">Home</v-tab>
+        <v-tab link to="/">Log out</v-tab>
         <v-tab link to="/profilevis">Perfil</v-tab>
       </v-tabs>
     </v-card>
